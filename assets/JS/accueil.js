633726d62cc048e9f1d6699b6f8efc5e23e46cbb
Kylian Baneman <kylianbaneman@gmail.com>
2025-03-04 16:05:22 +0100

    animations hover

--- assets/JS/accueil.js
+++ assets/JS/accueil.js
@@ -2,7 +2,7 @@ let onglets = document.querySelectorAll(".header__menu li")
 
 onglets.forEach(onglet => onglet.addEventListener("mouseenter" , () =>{
   let lien = onglet.querySelector(".lien")
-  lien.style.transform = "scale(1.05)"
+  lien.style.transform = "scale(1.1)"
 }))
 onglets.forEach(onglet => onglet.addEventListener("mouseleave" , () =>{
   let lien = onglet.querySelector(".lien")
