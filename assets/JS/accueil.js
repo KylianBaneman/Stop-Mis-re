d305865a6bca50dee55c9301914e7fce95ad5ecd
Kylian Baneman <kylianbaneman@gmail.com>
2025-03-04 21:51:10 +0100

    responsive mobile

--- assets/JS/accueil.js
+++ assets/JS/accueil.js
@@ -1,5 +1,24 @@
 let onglets = document.querySelectorAll(".header__menu li")
+let menuBurgerIcon = document.querySelector(".burger--toggler")
+let menuBurger = document.querySelector(".menuBurger")
+let Overlay = document.querySelector(".overlay--burger")
+let trigger = document.querySelector(".trigger")
 
+menuBurgerIcon.addEventListener("click" , toggleBurger)
+
+function toggleBurger(){
+  menuBurgerIcon.classList.toggle("active")
+  menuBurger.classList.toggle("active")
+  Overlay.classList.toggle("active")
+}
+
+trigger.addEventListener("click" , toggleclose)
+
+function toggleclose(){
+  menuBurgerIcon.classList.toggle("active")
+  menuBurger.classList.toggle("active")
+  Overlay.classList.toggle("active")
+}
 
 onglets.forEach(onglet => onglet.addEventListener("mouseenter" , () =>{
   let lien = onglet.querySelector(".lien")
