23ebf5b2450d95c88951db5e5b7f13cdc96d4e15
Kylian Baneman <kylianbaneman@gmail.com>
2025-03-04 22:47:41 +0100

    header scroll animation

--- assets/JS/accueil.js
+++ assets/JS/accueil.js
@@ -1,8 +1,13 @@
 let onglets = document.querySelectorAll(".header__menu li")
+let navBar = document.querySelector(".header__menu")
 let menuBurgerIcon = document.querySelector(".burger--toggler")
 let menuBurger = document.querySelector(".menuBurger")
 let Overlay = document.querySelector(".overlay--burger")
 let trigger = document.querySelector(".trigger")
+let lastScrollTop = 0
+let headerHidden = false
+
+window.addEventListener("scroll" , scrollHeader)
 
 menuBurgerIcon.addEventListener("click" , toggleBurger)
 
@@ -28,3 +33,16 @@ onglets.forEach(onglet => onglet.addEventListener("mouseleave" , () =>{
   let lien = onglet.querySelector(".lien")
   lien.style.transform = "scale(1)"
 }))
+
+function scrollHeader(){
+  let scrollTop = window.pageYOffset || document.documentElement.scrollTop
+  if (scrollTop > lastScrollTop && scrollTop > 50) {
+    // Si on scrolle vers le bas et qu'on a dépassé 50px, cache le header
+    navBar.style.top = "-100px";
+    headerHidden = true; // On marque que le header a été caché
+  } else if (scrollTop < lastScrollTop && headerHidden) {
+    // Si on remonte ET que le header avait été caché, on l'affiche
+    navBar.style.top = "40px";
+  }
+  lastScrollTop = scrollTop
+}
