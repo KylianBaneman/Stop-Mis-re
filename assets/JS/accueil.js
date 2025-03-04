928247f3b1c0de56f4c895ab7eefce48a2f24124
Kylian Baneman <kylianbaneman@gmail.com>
2025-03-04 16:40:36 +0100

    button

--- assets/JS/accueil.js
+++ assets/JS/accueil.js
@@ -1,4 +1,13 @@
 let onglets = document.querySelectorAll(".header__menu li")
+let btn = document.querySelector(".fond__texte__button button")
+let txtBtn = btn.textContent
+
+btn.addEventListener("mouseenter" , () =>{
+  txtBtn.style.transform = "scale(1.1)"
+})
+btn.addEventListener("mouseleave" , () =>{
+  txtBtn.style.transform = "scale(1)"
+})
 
 onglets.forEach(onglet => onglet.addEventListener("mouseenter" , () =>{
   let lien = onglet.querySelector(".lien")
