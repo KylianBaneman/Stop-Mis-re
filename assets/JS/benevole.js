c8c39d7f863186e16a552f8a0923ed8cecfc50ee
Kylian Baneman <kylianbaneman@gmail.com>
2025-03-06 10:06:16 +0100

    java script form and animations on the form

--- assets/JS/benevole.js
+++ assets/JS/benevole.js
@@ -1,19 +1,15 @@
 let Overlay_modal = document.querySelector(".overlay--modal")
 let trigger_modal = document.querySelector(".trigger--modal")
 let Modal = document.querySelector(".modal")
-let btn = document.querySelector(".button")
-let inps = document.querySelectorAll(".contact input")
+let btn = document.querySelector(".form__content__btn button")
+let inps = document.querySelectorAll(".content__firstCol input")
 let tarea = document.querySelector("textarea")
 let form = document.querySelector("form")
 
-form.addEventListener("submit" , toggleContact)
-trigger_modal.addEventListener("click" , toggleContactClose)
 
-function toggleContactClose(){
-  Overlay_modal.classList.toggle("active")
-  Modal.classList.toggle("active")
-}
-function toggleContact(e){
+form.addEventListener("submit" , submitForm)
+
+function submitForm(e){
   e.preventDefault()
   let envoie = true
   inps.forEach(inp=>{
